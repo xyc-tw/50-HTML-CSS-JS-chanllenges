61599e1eff3c40969c97cbff4a57056536dd792b
xychen <mars_0608@hotmail.com>
2022-01-13 15:25:32 +0100

    first example

---
+++ 01_Expanding_cards/script.js
@@ -0,0 +1,20 @@
+{
+  ("use strict");
+
+  const panels = document.querySelectorAll(".panel");
+  console.log(panels);
+  console.log(2 + 2);
+
+  panels.forEach((panel) => {
+    panel.addEventListener("click", () => {
+      removeActiveClasses();
+      panel.classList.add("active");
+    });
+  });
+
+  function removeActiveClasses() {
+    panels.forEach((panel) => {
+      panel.classList.remove("active");
+    });
+  }
+}
